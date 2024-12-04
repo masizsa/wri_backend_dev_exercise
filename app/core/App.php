d53389d9b43157a8f61b15373f7ce6e5a492d97a
SyahrulBhudiF <syahrul4w@gmail.com>
2024-12-04 12:18:42 +0700

    fix: APP

--- app/core/App.php
+++ app/core/App.php
@@ -8,41 +8,34 @@ class App
 
     public function __construct()
     {
-        // $url = $this->parseUrl();
-        // if (!empty($url)) {
-        //     if (file_exists('../app/controllers/' . $url[0] . ".php")) {
-        //         $this->controller = $url[0];
-        //         unset($url[0]);
-        //     } else {
-        //         echo 'not found';
-        //     }
-        //     require_once '../app/controllers/' . $this->controller . '.php';
+        $url = $this->parseURL();
 
-        //     $this->controller = new $this->controller;
+        // controller
+        if (isset($url[0])) {
+            if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+                $this->controller = $url[0];
+                unset($url[0]);
+            }
+        }
 
-        //     if (isset($url[1])) {
-        //         $methodName = $url[1];
-        //         if (method_exists($this->controller, $methodName)) {
-        //             $this->method = $methodName;
-        //             unset($url[1]);
-        //         }
-        //     }
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
 
-        //     $this->params = array_values($url);
-        //     call_user_func_array([$this->controller, $this->method], $this->params);
-        // } else {
-        //     // Limit access to only the login controller if the user is not logged in
-        //     require_once '../app/controllers/home.php';
-        //     // $this->controller = new Login();
+        // method
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
 
-        //     if (isset($url[1]) && $url[0] === 'login' && method_exists($this->controller, $url[1])) {
-        //         $this->method = $url[1];
-        //         unset($url[1]);
-        //     }
+        // params
+        if (!empty($url)) {
+            $this->params = array_values($url);
+        }
 
-        //     $this->params = array_values($url);
-        //     call_user_func_array([$this->controller, $this->method], $this->params);
-        // }
+        // jalankan controller & method, serta kirimkan params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
 
     }
     public function parseUrl()
